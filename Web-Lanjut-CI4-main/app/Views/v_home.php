47d87fca0ab1b7926d0242394566b8f3646cab51
M Hamid Al Malik - 1617051082 <74697047+muhamidam@users.noreply.github.com>
2021-10-21 11:19:59 +0700

    Delete Web-Lanjut-CI4-main/app directory

--- Web-Lanjut-CI4-main/app/Views/v_home.php
+++
@@ -1,3 +0,0 @@
-<div class="container mt-3">
-    <h1 class="text-center">Selamat Datang di Praktikum Web Lanjut</h1>
-</div>
--- Web-Lanjut-CI4-main/app/Views/v_posts.php
+++
@@ -1,5 +0,0 @@
-<div class="container mt-3">
-    <div class="text-center">
-        <h1>All posts</h1>
-    </div>
-</div>
