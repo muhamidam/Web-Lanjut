102ab84f61789acee405923bf2d96aac75ee7c4a
M Hamid Al Malik - 1617051082 <74697047+muhamidam@users.noreply.github.com>
2021-10-21 11:23:28 +0700

    Add files via upload

---
+++ Web-Lanjut-CI4-main/app/Controllers/Templating.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\UserModel;
+
+class Templating extends BaseController
+{
+	public function __construct()
+	{
+		$this->userModel = new UserModel();
+	}
+	public function index()
+	{
+		$data = [
+			'title' => "Blog - Posts",
+		];
+		// echo view('layouts/header', $data);
+		// echo view('layouts/navbar');
+		// echo view('v_posts');
+		// echo view('layouts/footer');
+        return view('view_admin', $data);
+	}
+
+	public function register()
+	{
+		$data = [
+			'title' => 'Register',
+		];
+		// echo view('layouts/header', $data);
+		// echo view('layouts/navbar');
+		// echo view('v_posts');
+		// echo view('layouts/footer');
+        return view('v_register', $data);
+	}
+
+	public function saveRegister()
+	{
+		$request = service ('request');
+		$data = [
+			'fullname' => $request->getVar('fullname'),
+			'email' => $request->getVar('email'),
+			'password' => $request->getVar('password'),
+		];
+		$this->userModel->insert($data);
+		return redirect()->to('/register');
+	}
+}
